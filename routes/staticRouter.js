6c9fccbd18a17cf869f1ae5adc472676587b462a
Vivek Maurya <vivekmaurya030gmail.com>
2024-04-24 15:39:37 +0530

    router problem

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,13 +3,21 @@ const URL = require("../models/url");
 
 const router = express.Router();
 
-router.get('/',async(req,res)=>{
+router.get(['/','/url'],async(req,res)=>{
     if(!req.user) return res.redirect("/login");
     const allUrls= await URL.find({createdBy: req.user._id});
     return res.render("home",{
         urls: allUrls,
     });
 });
+
+// router.get("/url",async(req,res)=>{
+//     if(!req.user) return res.redirect("/login");
+//     const allUrls= await URL.find({createdBy: req.user._id});
+//     return res.render("home",{
+//         urls: allUrls,
+//     });
+// });
 
 router.get("/signup",(req,res)=>{
     return res.render("signup");
